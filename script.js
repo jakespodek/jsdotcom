da244ab45e1dc204bff65cbce4e184f45d9b8488
Jake Spodek <jake.spodek1@gmail.com>
2021-06-02 21:22:57 -0400

    form submission function test

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hire me!');
+const app = {};
+
+app.SelectForm = () => {
+    document.querySelector("form").addEventListener("submit", app.handleSubmit);
+}
+
+app.handleSubmit = (event) => {
+    event.preventDefault();
+    const button = document.querySelector("button");
+    button.innerHTML = "Sent!";
+    button.style.backgroundColor('salmon');
+    button.style.color('black');
+};
+
+app.init = () => {
+    console.log('Hire me!');
+    app.SelectForm();
+}
