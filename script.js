e1ec2ed9f9a35fb405d233c3152d6169d67c4d71
Jake Spodek <jake.spodek1@gmail.com>
2021-07-22 17:33:02 -0400

    design stuff

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('%c Hire me!', 'color: #40FF40; font-weight: bold; font-size: 16px');
+const app = {};
+
+app.mobileNav = () => {
+    document.getElementById("mobileNavButton").addEventListener("click", () => {
+        console.log('button!');
+    });
+}
+
+app.init = () => {
+    console.log('%c Hire me!', 'color: #40FF40; font-weight: bold; font-size: 16px');
+    app.mobileNav();
+};
+
+app.init();
+
